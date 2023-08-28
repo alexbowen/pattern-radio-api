1c56ac59758820ff2a3471b6b55fe39238af43bb
alexbowen <alexbowen123@googlemail.com>
2023-08-28 15:07:40 +0100

    database setup

--- db/migrate/20220820112035_create_journeys.rb
+++
@@ -1,7 +0,0 @@
-class CreateJourneys < ActiveRecord::Migration[7.0]
-  def change
-    create_table :journeys, id: :uuid do |t|
-      t.timestamps
-    end
-  end
-end
--- db/migrate/20220902100954_add_geo_travel_areas_to_journeys.rb
+++
@@ -1,5 +0,0 @@
-class AddGeoTravelAreasToJourneys < ActiveRecord::Migration[7.0]
-  def change
-    add_column :journeys, :travel_area, :geometry, geographic: true, srid: 4055
-  end
-end
