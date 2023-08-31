d7b6d01bea04a4559dc7b3cb9444a6c45a82b7b8
alexbowen <alexbowen123@googlemail.com>
2023-08-31 12:05:31 +0100

    allow tags to be searchable

--- app/controllers/api/search_controller.rb
+++ app/controllers/api/search_controller.rb
@@ -1,13 +1,19 @@
 class Api::SearchController < ApplicationController
   def index
     if params[:q].length > 0
-      result = Show.search(params[:q]).order("created_time DESC").limit(params[:limit]).offset(params[:offset]).reverse
+      shows1 = Show.search(params[:q]).order("created_time DESC").limit(params[:limit]).offset(params[:offset]).reverse
+      shows2 = Show.select { |show| show[:tags].any?{|tag| tag["name"].downcase.include? params[:q] } }
+
+      shows = (shows1 + shows2).uniq
+
+      count = shows.length
+      shows = shows.slice(params[:offset].to_i, params[:limit].to_i)
     else
-      result = Show.all
+      shows = Show.all
     end
 
-    count = result.length
+    count = shows.length
 
-    render json: { count: count, shows: result }
+    render json: { count: count, shows: shows }
   end
 end
