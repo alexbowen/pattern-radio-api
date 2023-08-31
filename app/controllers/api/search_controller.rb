408178d74c9b3c473d123de7013d9d8b4a6bf78c
alexbowen <alexbowen123@googlemail.com>
2023-08-31 13:08:44 +0100

    fix search only fetching first page of results

--- app/controllers/api/search_controller.rb
+++ app/controllers/api/search_controller.rb
@@ -1,7 +1,7 @@
 class Api::SearchController < ApplicationController
   def index
     if params[:q].length > 0
-      shows1 = Show.search(params[:q]).order("created_time DESC").limit(params[:limit]).offset(params[:offset]).reverse
+      shows1 = Show.search(params[:q]).order("created_time DESC").reverse
       shows2 = Show.select { |show| show[:tags].any?{|tag| tag["name"].downcase.include? params[:q] } }
 
       shows = (shows1 + shows2).uniq
@@ -10,9 +10,8 @@ class Api::SearchController < ApplicationController
       shows = shows.slice(params[:offset].to_i, params[:limit].to_i)
     else
       shows = Show.all
+      count = shows.length
     end
-
-    count = shows.length
 
     render json: { count: count, shows: shows }
   end
